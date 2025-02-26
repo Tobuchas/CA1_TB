30bfc19fdb35e385742cc25069538e92625f60dc
Tobuchas <tom@buchtovi.cz>
2025-02-27 00:11:17 +0100

    Connecting the CSV file to the main

--- main.cpp
+++ main.cpp
@@ -1,25 +1,92 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+using namespace std;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
+struct Lizard {
+    string species;
+    float length;
+    string habitat;
+    string diet;
+    int age;
+};
 
-    return 0;
+void display(const Lizard &liz)
+{
+    cout <<left
+    << setw(30)<< liz.species
+    <<setw(10) <<liz.length
+    <<setw(15)<< liz.habitat
+    <<setw(15)<<liz.diet
+    <<setw(5)<<liz.age
+    <<endl;
+
 }
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+void displayAll(vector<Lizard> &liz) {
+    cout <<" Species\t\t      Length\tHabitat\t       Diet\t      Age"<<endl;
+    cout <<"_________________________________________________________________________"<<endl;
+    for(int i = 0; i < liz.size(); i++) {
+        display(liz[i]);
+    }
+}
+
+void parseLine(string line,Lizard &liz)
+{
+
+    string temp;
+    stringstream ss(line);
+    getline(ss, liz.species, ',');
+    getline(ss, temp, ',');
+    liz.length = stod(temp);
+
+    getline(ss, liz.habitat, ',');
+    getline(ss, liz.diet, ',');
+    getline(ss, temp, ',');
+    liz.age = stoi(temp);
+
+}
+
+void load(const string &fname, vector<Lizard> &data)
+{
+    ifstream fin(fname);
+
+    if(fin)
+    {
+        string line;
+        getline(fin, line);
+
+        while(getline(fin, line))
+        {
+            Lizard liz;
+            parseLine(line, liz);
+            data.push_back(liz);
+        }
+        fin.close();
+    }
+    else
+    {
+        cout << "Error opening file." <<endl;
+    }
+}
+
+int main() {
+    // Lizard lizard;
+    // lizard.species = "Crack";
+    // lizard.length = 3;
+    // lizard.habitat = "Canada";
+    // lizard.diet = "Doodoo";
+    // lizard.age = 20;
+    // display(lizard);
+
+    vector<Lizard> data;
+    load("lizardsTable.csv", data);
+
+
+    displayAll(data);
+}
